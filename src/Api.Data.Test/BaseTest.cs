b64cc6e0aae34070f7aa2e0fe63cf344fc92d411
Ricardo G <ricardo.gomes135@gmail.com>
2020-08-31 22:31:47 -0300

    testes unitários com XUnit - Api.Data

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+
+    }
+
+    public class DbTeste : IDisposable
+    {
+
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(
+                o => o.UseMySql($"Persist Security Info=True;Server=localhost;Database={dataBaseName};User=root;Password=ra135jx420"),
+                ServiceLifetime.Transient);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -15,6 +15,17 @@ namespace Api.Data.Context
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+
+            modelBuilder.Entity<UserEntity>().HasData(
+                new UserEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Admin",
+                    Email = "admin@email.com",
+                    CreateAt = DateTime.Now,
+                    UpdateAt = DateTime.Now,
+                }
+            );
         }
     }
 }
